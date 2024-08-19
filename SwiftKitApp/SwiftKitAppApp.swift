dfc22760bdbc4c9c0eac9db87914f43893bb9347
Mikhail <chelbaev.mihail70@gmail.com>
2024-08-19 16:59:54 +0300

    chore: create project

---
+++ SwiftKitApp/SwiftKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftKitAppApp.swift
+//  SwiftKitApp
+//
+//  Created by Mikhail on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
