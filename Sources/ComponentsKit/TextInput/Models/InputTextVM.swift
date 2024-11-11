8fcbbbcc2c2878f92be4c92734822c8eac2205d6
Mikhail <chelbaev.mihail70@gmail.com>
2024-11-11 15:34:30 +0100

    handle case when number of rows in textinput is less than 1

--- Sources/ComponentsKit/TextInput/Models/InputTextVM.swift
+++ Sources/ComponentsKit/TextInput/Models/InputTextVM.swift
@@ -218,6 +218,8 @@ extension InputTextVM {
     }
   }
   private func height(forRows rows: Int) -> CGFloat {
+    // TODO: [2] Show a warning if number of rows less than 1
+    let numberOfRows = max(1, rows)
     return self.preferredFont.uiFont.lineHeight * CGFloat(rows) + 2 * self.contentPadding
   }
 }
