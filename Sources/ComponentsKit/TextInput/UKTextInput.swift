dccfd86a7fe4eb04234a0b56f67321d99b1ed0d6
Vislov Ivan <vislov.ivan@bk.ru>
2024-11-12 08:54:13 +0300

    add placeholder

--- Sources/ComponentsKit/TextInput/UKTextInput.swift
+++ Sources/ComponentsKit/TextInput/UKTextInput.swift
@@ -23,6 +23,8 @@ open class UKTextInput: UIView, UKComponent, UITextViewDelegate {
 
   public var textView = UITextView()
 
+  private var placeholderLabel = UILabel()
+
   // MARK: UIView Properties
 
   open override var intrinsicContentSize: CGSize {
@@ -62,10 +64,11 @@ open class UKTextInput: UIView, UKComponent, UITextViewDelegate {
 
   private func setup() {
     self.addSubview(self.textView)
+    self.addSubview(self.placeholderLabel)
     self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleTap)))
     self.textView.delegate = self
     self.textView.isScrollEnabled = false
-    self.applyPlaceholderIfNeeded()
+    self.placeholderLabel.isUserInteractionEnabled = false
   }
 
   @objc private func handleTap() {
@@ -75,27 +78,27 @@ open class UKTextInput: UIView, UKComponent, UITextViewDelegate {
   // MARK: UITextViewDelegate
 
   public func textViewDidBeginEditing(_ textView: UITextView) {
-    if textView.text == model.placeholder {
-      textView.text = ""
-      textView.textColor = model.foregroundColor.uiColor
-    }
-  }
-
-  public func textViewDidEndEditing(_ textView: UITextView) {
-    if textView.text.isEmpty {
-      applyPlaceholderIfNeeded()
-    }
+    updatePlaceholderVisibility()
   }
 
   public func textViewDidChange(_ textView: UITextView) {
     self.onValueChange(textView.text)
     self.adjustTextViewHeight()
+    updatePlaceholderVisibility()
+  }
+
+  public func textViewDidEndEditing(_ textView: UITextView) {
+    updatePlaceholderVisibility()
+  }
+
+  private func updatePlaceholderVisibility() {
+    placeholderLabel.isHidden = !textView.text.isEmpty
   }
 
   private func adjustTextViewHeight() {
     let estimatedSize = self.textView.sizeThatFits(CGSize(width: self.textView.frame.width, height: CGFloat.infinity))
-    let minHeight = model.calculatedHeight(forRows: model.minRows + 1)
-    let maxHeight = model.maxRows.map { model.calculatedHeight(forRows: $0 + 1) } ?? .greatestFiniteMagnitude
+    let minHeight = model.minTextInputHeight
+    let maxHeight = model.maxTextInputHeight
 
     let newHeight = min(max(estimatedSize.height, minHeight), maxHeight)
     self.textViewHeightConstraint?.constant = newHeight
@@ -107,7 +110,11 @@ open class UKTextInput: UIView, UKComponent, UITextViewDelegate {
   private func style() {
     Self.Style.mainView(self, model: self.model)
     Self.Style.textView(self.textView, model: self.model)
-    applyPlaceholderIfNeeded()
+
+    placeholderLabel.font = model.preferredFont.uiFont
+    placeholderLabel.textColor = model.placeholderColor.uiColor
+    placeholderLabel.text = model.placeholder
+    placeholderLabel.isHidden = !textView.text.isEmpty
   }
 
   private func applyPlaceholderIfNeeded() {
@@ -120,20 +127,31 @@ open class UKTextInput: UIView, UKComponent, UITextViewDelegate {
   // MARK: Layout
 
   private func layout() {
-    self.textView.leading(self.model.horizontalPadding)
-    self.textView.trailing(self.model.horizontalPadding)
-    self.textView.vertically()
+      self.textView.leading()
+      self.textView.trailing()
+      self.textView.vertically()
 
-    let minHeight = model.calculatedHeight(forRows: model.minRows)
-    self.textViewHeightConstraint = self.textView.heightAnchor.constraint(equalToConstant: minHeight)
-    self.textViewHeightConstraint?.isActive = true
+      placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+          placeholderLabel.leadingAnchor.constraint(equalTo: textView.leadingAnchor, constant: model.contentPadding),
+          placeholderLabel.topAnchor.constraint(equalTo: textView.topAnchor, constant: textView.textContainerInset.top + 4)
+      ])
 
-    self.textView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+      let minHeight = model.minTextInputHeight
+      self.textViewHeightConstraint = self.textView.heightAnchor.constraint(equalToConstant: minHeight)
+      self.textViewHeightConstraint?.isActive = true
+
+      self.textView.setContentHuggingPriority(.defaultLow, for: .horizontal)
   }
 
   open override func layoutSubviews() {
-    super.layoutSubviews()
-    self.layer.cornerRadius = self.model.cornerRadius.value(for: self.bounds.height)
+      super.layoutSubviews()
+
+      self.layer.cornerRadius = self.model.cornerRadius.value(for: self.bounds.height)
+
+      let midX = self.bounds.midX - self.textView.frame.width / 2
+      let midY = self.bounds.midY - self.textView.frame.height / 2
+      self.textView.frame.origin = CGPoint(x: midX, y: midY)
   }
 
   // MARK: Update
@@ -172,7 +190,7 @@ extension UKTextInput {
       model: TextInputVM
     ) {
       view.backgroundColor = model.backgroundColor.uiColor
-      view.layer.cornerRadius = model.cornerRadius.value(for: view.bounds.height)
+      view.layer.cornerRadius = model.adaptedCornerRadius.value(for: view.bounds.height)
     }
     static func textView(
       _ textView: UITextView,
