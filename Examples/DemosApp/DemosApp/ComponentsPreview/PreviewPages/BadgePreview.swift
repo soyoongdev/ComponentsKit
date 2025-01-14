d53eef5278f16e7477c6f059fcfc4352e2ed4539
Vislov Ivan <vislov.ivan@bk.ru>
2025-01-14 21:03:33 +0300

    added paddings picker into preview

--- Examples/DemosApp/DemosApp/ComponentsPreview/PreviewPages/BadgePreview.swift
+++ Examples/DemosApp/DemosApp/ComponentsPreview/PreviewPages/BadgePreview.swift
@@ -32,6 +32,14 @@ struct BadgePreview: View {
           Text("Filled").tag(BadgeVM.Style.filled)
           Text("Light").tag(BadgeVM.Style.light)
         }
+        Picker("Paddings", selection: self.$model.paddings) {
+          Text("8px; 6px")
+            .tag(Paddings(top: 6, leading: 8, bottom: 6, trailing: 8))
+          Text("10px; 8px")
+            .tag(Paddings(top: 8, leading: 10, bottom: 8, trailing: 10))
+          Text("12px; 10px")
+            .tag(Paddings(top: 10, leading: 12, bottom: 10, trailing: 12))
+        }
       }
     }
   }
