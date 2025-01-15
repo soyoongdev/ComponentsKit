6ca505412a867af5802fbddb9ba0e79e5065b3c9
Mikhail <chelbaev.mihail70@gmail.com>
2025-01-15 16:59:51 +0100

    remove unused helper

--- Sources/ComponentsKit/Components/Avatar/Models/AvatarVM.swift
+++ Sources/ComponentsKit/Components/Avatar/Models/AvatarVM.swift
@@ -129,13 +129,3 @@ extension AvatarVM {
     }
   }
 }
-
-// MARK: - UIKit Helpers
-
-extension AvatarVM {
-  func shouldUpdateImage(_ oldModel: Self) -> Bool {
-    return self.imageSrc != oldModel.imageSrc
-    || self.placeholder != oldModel.placeholder
-    || self.color != oldModel.color
-  }
-}
